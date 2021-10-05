c0b85b42c4ffaaceebf7a0ff6adb79419285f53f
JoshMas <49138669+JoshMas@users.noreply.github.com>
2021-10-05 16:34:59 +1100

    Made basic menu
    Started on a game manager (not sure we'll use this one)

---
+++ Assets/Joshua/Scripts/Core/GameManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    #region singleton
+    public static GameManager Instance;
+    private void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else if(Instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+    #endregion
+
+    private int currentRoom = 0;
+    [SerializeField] private int maxRooms = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
