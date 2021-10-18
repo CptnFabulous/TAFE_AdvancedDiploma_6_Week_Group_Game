2933e0d708404d3142a745dbd3e2339d97fbbc55
Marcus Mouyat <marcus@mouyat.com>
2021-10-18 18:18:54 +1100

    Fixed player view obstruction problems
    Tried figuring out a solution to prevent the walls from obstructing the player’s vision. I tried making a script to fix it, but after it failed to work I went for a simpler but highly effective solution of simply shrinking the walls.

---
+++ Assets/Scripts/ThirdPersonCameraObstructionHider.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonCameraObstructionHider : MonoBehaviour
+{
+    public GameObject player;
+    public float viewingAngle = 45;
+    //public float padding = 5;
+
+    
+
+    List<Renderer> currentRenderers = new List<Renderer>();
+    Renderer[] playerRenderers;
+    Camera playerCamera;
+
+
+    private void Awake()
+    {
+        playerRenderers = player.GetComponentsInChildren<Renderer>();
+        playerCamera = GetComponent<Camera>();
+    }
+
+    private void LateUpdate()
+    {
+        //Gizmos.DrawSphere(transform.position, Vector3.Distance(transform.position, player.transform.position));
+        Collider[] collidersBetween = Physics.OverlapSphere(transform.position, Vector3.Distance(transform.position, player.transform.position), playerCamera.cullingMask);
+        for (int i = 0; i < collidersBetween.Length; i++)
+        {
+            currentRenderers.AddRange(collidersBetween[i].GetComponentsInChildren<Renderer>());
+        }
+
+        // Neatly check through every renderer in the list
+        currentRenderers.RemoveAll(r => IsRendererSafeFromCulling(r));
+
+
+        // Now that we have the list of current renderers in the radius
+
+
+
+
+    }
+
+    bool IsRendererSafeFromCulling(Renderer r)
+    {
+        for (int i = 0; i < playerRenderers.Length; i++)
+        {
+            if (playerRenderers[i] == r)
+            {
+                return true;
+            }
+        }
+
+        //if (r.dis)
+        
+        Vector3 direction = player.transform.position - transform.position;
+        float distance = Vector3.Distance(transform.position, r.bounds.center);
+        Vector3 closestPointDirection = r.bounds.ClosestPoint(transform.position) - transform.position;
+
+        bool outside = Vector3.Angle(direction, closestPointDirection) > viewingAngle;
+        r.enabled = outside;
+        return outside;
+    }
+}
